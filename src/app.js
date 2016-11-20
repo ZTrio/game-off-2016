c7982ef550924fbfeff2f9b79752a5f744076432
kevzettler <kevzettler@gmail.com>
2016-11-20 10:12:30 -0800

    almost ground

--- src/app.js
+++ src/app.js
@@ -17,11 +17,16 @@ class Editor extends React.Component {
   constructor(props, context){
     super(props, context);
     this.cameraPosition = new THREE.Vector3(0, 25, 50);
+
     this.directionalLightColor = new THREE.Color( 0xffffff );
-    this.directionalLightColor.setHSL(01,1,0.95);
+    this.directionalLightColor.setHSL(0.1,1,0.95);
     this.directionalLightPosition = new THREE.Vector3(10, 10.75, 10);
     this.directionalLightPosition.multiplyScalar(10);
+
     this.groundScale = new THREE.Vector3(1000, 1000, 1000);
+    this.groundTexRepeat = new THREE.Vector2(512*3, 512*3);
+    this.groundRotation = new THREE.Euler();
+    this.groundRotation.x = - Math.PI / 2;
     this.state = {};
   }
 
@@ -61,7 +66,7 @@ class Editor extends React.Component {
                 castShadow={true}
                 shadowMapWidth={2048}
                 shadowMapHeight={2048}
-                sahadowCameraLeft={-150}
+                shadowCameraLeft={-150}
                 shadowCameraRight={150}
                 shadowCameraTop={150}
                 shadowCameraBottom={-150}
@@ -76,8 +81,19 @@ class Editor extends React.Component {
               />
               <meshBasicMaterial color={0x00ff00}/>
             </mesh>
-            <mesh name="ground" scale={this.groundScale}}>
-              <planeBufferGeometry width="100" height="100"/>
+            <mesh name="ground"
+                  scale={this.groundScale}
+                  rotation={this.groundRotation}>
+              <planeBufferGeometry width={100} height={100}/>
+              <meshPhongMaterial color={0xFFFFFF}>
+                <texture
+                    url="./assets/textures/grass.png"
+                    anisotropy={1}
+                    repeat={this.groundTexRepeat}
+                    wrapS={THREE.RepeatWrapping}
+                    wrapT={THREE.RepeatWrapping}
+                />
+              </meshPhongMaterial>
             </mesh>
           </scene>          
         </React3>
