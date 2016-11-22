b96b70e8c9d43c1a3c3b62edc6e448ccef55bc5e
kevzettler <kevzettler@gmail.com>
2016-11-21 16:54:39 -0800

    redux state hookup

--- src/app.js
+++ src/app.js
@@ -2,13 +2,77 @@ import React from 'react';
 import React3 from 'react-three-renderer';
 import ReactDOM from 'react-dom';
 import THREE from "three";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import VoxLoader from './VoxLoader/Vox.js';
 import GameLoop from 'fixed-game-loop';
 import KeyDrown from 'keydrown';
 import modelNames from './modelNames.json';
 import Editor from './components/Editor';
 
+
 const OrbitControls = require('three-orbit-controls')(THREE);
+const width = window.innerWidth; // canvas width
+const height =  window.innerHeight; // canvas height
+
+const directionalLightColor = new THREE.Color( 0xffffff );
+directionalLightColor.setHSL(0.1,1,0.95);
+const directionalLightPosition = new THREE.Vector3(10, 10.75, 10);
+directionalLightPosition.multiplyScalar(10);
+
+const hemisphereLightSkyColor = new THREE.Color(0xffffff);
+hemisphereLightSkyColor.setHSL(0.6, 1, 0.6);
+const hemisphereLightGroundColor = new THREE.Color(0xffffff);
+hemisphereLightGroundColor.setHSL(0.095, 1, 0.75);
+
+const initialState = {
+  viewport: {
+    height,
+    width, 
+    alpha: true,
+    antialias: true,
+    shadoMapEnabled: true,
+    clearColor: 0x7ccaff,
+    clearAlpha: 1
+  },
+
+  camera: {
+    fov: 75,
+    aspect: width / height,
+    near: 0.1,
+    far: 1000,
+    position: new THREE.Vector3(0, 25, 50),
+  },
+
+  lights: {
+    ambientLight: {
+      color: 0x000033
+    },  
+
+    directionalLight: {
+      color: directionaLightColor,
+      position: directionalLightPosition,
+      castShadow: true,
+      shadowMapWidth: 2048,
+      shadowMapHeight: 2048,
+      shadowCameraLeft: -150,
+      shadowCameraRight: 150,
+      shadowCameraTop: 150,
+      shadowCameraBottom: -150,
+      shadowCameraFar: 3500,
+      shadowBias: -0.0001,
+    },
+
+    hemisphereLight: {
+      skyColor: hemisphereLightSkyColor,
+      groundColor: hemisphereLightGroundColor,
+      position: new THREE.Vector3(0, 500, 0),
+    }
+  }
+  
+};
+
+let store = createStore({}, initialState);
 
 class EditorContainer extends React.Component {
   constructor(props, context){
@@ -17,14 +81,14 @@ class EditorContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <Provider store={store} >
         <div id="uiContainer">
           <select>
-            {modelNames.map((modelName) => { return <option value={modelName} key={modelName}>{modelName}</option>})}
+            {modelNames.map((modelName) => { return <option value={modelName} key={modelName}>{modelName}</option>}) }
           </select>
         </div>
         <Editor />
-      </div>
+      </Provider>
     );
   }
 }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,55 +4,23 @@ import ReactDOM from 'react-dom';
 import THREE from "three";
 import Ground from './Ground';
 import Anchor from  './Anchor';
+import { connect } from 'react-redux';
 
-export default class Editor extends React.Component {
-  constructor(props, context){
-    super(props, context);
-    this.cameraPosition = new THREE.Vector3(0, 25, 50);
-  }
-
+class Editor extends React.Component {
   render(){
-    const width = window.innerWidth; // canvas width
-    const height = window.innerHeight; // canvas height    
+    const {
+      viewport,
+      camera,
+      lights
+    } = props;
     
     return (
-      <React3
-          clearColor={0x7ccaff}
-          clearAlpha={1}
-          alpha={true}
-          shadowMapEnabled={true}
-          antialias={true}
-          width={width}
-          height={height}
-          mainCamera="camera">
+      <React3 {...viewport} mainCamera="camera">
         <scene>
-          <perspectiveCamera
-              name="camera"
-              fov={75}
-              aspect={width / height}
-              near={0.1}
-              far={1000}
-              position={this.cameraPosition}
-          />
-          <ambientLight color={0x000033} />
-          <directionalLight
-              color={this.directionaLightColor}
-              position={this.directionalLightPosition}
-              castShadow={true}
-              shadowMapWidth={2048}
-              shadowMapHeight={2048}
-              shadowCameraLeft={-150}
-              shadowCameraRight={150}
-              shadowCameraTop={150}
-              shadowCameraBottom={-150}
-              shadowCameraFar={3500}
-              shadowBias={-0.0001}
-          />
-          <hemisphereLight
-              skyColor={this.hemisphereLightSkyColor}
-              groundColor={this.hemisphereLightGroundColor}
-              position={this.hemisphereLightPosition}
-          />
+          <perspectiveCamera name="camera" {...camera} />
+          {Object.keys(lights).map((lightName) =>{
+             return <lightName {...lights[lightName]} />;
+           })}
           <axisHelper size={10}/>
           <Anchor />
           <Ground />
@@ -61,3 +29,17 @@ export default class Editor extends React.Component {
     );
   }
 };
+
+Editor.propTypes = {
+  camera: React.PropTypes.object,
+};
+
+export function mapStateToProps(state, ownProps) {
+  return{
+    viewport: state.viewport,
+    camera: state.camera,
+    lights: state.lights
+  };
+}
+
+export default connect(mapStateToProps)(Editor);
